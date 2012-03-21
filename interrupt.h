03553a2dc07f2771c8a7093a55a48c54980bfe49
Ryan <ryan@krabath.net>
2012-03-21 01:37:09 -0500

    PCINT resources

---
+++ interrupt.h
@@ -0,0 +1,9 @@
+Some links to get this started:
+
+http://www.avrfreaks.net/index.php?name=PNphpBB2&file=viewtopic&t=62216
+http://code.google.com/p/arduino-tiny/source/browse/trunk/libraries/PinChangeInterrupt/PinChangeInterrupt.cpp
+http://www.avrfreaks.net/index.php?name=PNphpBB2&file=viewtopic&t=105493
+
+We'll need to use PCINT10 to match what we've implemented in hardware for the MFP (1hz)
+
+Switches connected to PCINT8 and PCINT9
