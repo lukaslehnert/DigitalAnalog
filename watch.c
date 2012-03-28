e1f21cba23c80531ad5386073fea5a602fcc6a69
Ryan <ryan@krabath.net>
2012-03-27 22:37:29 -0500

    File cleanup.

--- watch.c
+++ watch.c
@@ -63,62 +63,6 @@ int main(void) {
 
     }
 
-    /*ret = i2c_start(I2CADDR+I2C_WRITE);       // set device address and write mode
-
-      if ( ret )
-      {
-    // failed to issue start condition, possibly no device found 
-    i2c_stop();
-    LEDflashAlert();    // Flash an alert signal to indicate that we have an invalid start condition.
-    LEDflashAlert();                        
-    }
-    else
-    {
-    // issuing start condition ok, device accessible 
-    i2c_write(0x00);                         // write address = 0
-    i2c_rep_start(I2CADDR+I2C_READ);        // set device address and read mode
-    ret = i2c_readNak();                       // read one byte form address 0
-    i2c_stop();                              // set stop condition = release bus
-
-
-    i2c_start_wait(I2CADDR+I2C_WRITE);       // set device address and write mode
-    i2c_write(0x00);                       // write address = 0
-    i2c_write(0b01010101);                       // ret=0 -> Ok, ret=1 -> no ACK 
-    i2c_stop();                            // set stop conditon = release bus
-
-    // write ok, read value back from eeprom address 0x05, wait until 
-    //            the device is no longer busy from the previous write operation 
-    i2c_start_wait(I2CADDR+I2C_WRITE);     // set device address and write mode
-    i2c_write(0x00);                        // write address = 0
-    i2c_rep_start(I2CADDR+I2C_READ);       // set device address and read mode
-    ret = i2c_readNak();                    // read one byte
-    i2c_stop();
-
-    LEDflashData(ret);
-
-    // write 0x70,0x71,072,073 to eeprom address 0x00..0x03 (Page Write),
-    //            wait until the device is no longer busy from the previous write operation 
-    //        i2c_start_wait(I2CADDR+I2C_WRITE);     // set device address and write mode
-    //       i2c_write(0x00);                        // write start address = 0
-    //       i2c_write(0x70);                        // write data to address 0
-    //       i2c_write(0x71);                        //    "    "   "    "    1
-    //       i2c_write(0x72);                        //    "    "   "    "    2
-    //       i2c_write(0x74);                        //    "    "   "    "    3
-    //       i2c_stop();                             // set stop conditon = release bus
-
-    // write ok, read value back from eeprom address 0..3 (Sequencial Read),
-    //            wait until the device is no longer busy from the previous write operation 
-    //      i2c_start_wait(I2CADDR+I2C_WRITE);      // set device address and write mode
-    //      i2c_write(0x00);                         // write address = 0
-    //      i2c_rep_start(I2CADDR+I2C_READ);        // set device address and read mode
-    //     ret = i2c_readAck();                       // read one byte form address 0
-    //    ret = i2c_readAck();                       //  "    "    "    "     "    1
-    //   ret = i2c_readAck();                       //  "    "    "    "     "    2
-    //  ret = i2c_readNak();                       //  "    "    "    "     "    3
-    // i2c_stop();                              // set stop condition = release bus
-    */
-
-    return 0;
 
 }
 
