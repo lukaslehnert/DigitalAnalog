aad852617033e134182c5fb6e29dca2254ab087e
Ryan <ryan@krabath.net>
2013-02-24 00:54:22 -0600

    Pulled in some additional constants pulled in from the other header files.

--- pindefs.h
+++ pindefs.h
@@ -1,17 +1,32 @@
-#define SDA PA6
-#define SCL PA4
+#define SDA             PA6
+#define SCL             PA4
 #define SDA_PORT        PORTA
 #define SCL_PORT        PORTA 
 
 #define DDR_USI         DDRA
 #define PIN_USI         PINA
 
-#define POS             PA1
-#define NEG             PA0
+#define POS             PA5
+#define NEG             PA7
 
 #define STATUSPORT      PORTA
 #define STATUSIOPORT    DDRA
 #define STATUSLED       PA7
 #define STATUSVCC       PA5
 
+#define FACEPORT        PORTA
+#define FACEIOPORT      DDRA
+
+#define MIN_DATA        PA3
+#define HOUR_DATA       PA3
+#define DATA            PA3
+#define HOUR_CLOCK      PA2
+#define MIN_CLOCK       PA0
+
+#define CONTROLPORT     PORTB
+#define CONTROLIOPORT   DDRB
+#define CONTROLPIN      PB2
+
+
+#define PWM_DUTY_CYCLE  OCR0A
 
